1ba5112b712270b29d587eac82b8e7375ca583a8
Dakota Walsh <kotawalsh@gmail.com>
2018-02-27 13:19:51 -0500

    add main.lua and 3 main callbacks

---
+++ main.lua
@@ -0,0 +1,17 @@
+--   Copyright (C) 2018 Dakota Walsh   --
+-- Songbird - A love2d engine extender --
+
+-- import class library
+local class = require 'lib/middleclass'
+
+-- love load callback
+function love.load()
+end
+
+-- love update callback
+function love.update()
+end
+
+-- love draw callback
+function love.draw()
+end
