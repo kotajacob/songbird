0045175401e965ccdae9bcc337d679b2cd47c260
Dakota Walsh <kotawalsh@gmail.com>
2018-02-27 20:56:32 -0500

    detect if room file exists and error on failure

--- room.lua
+++ room.lua
@@ -6,7 +6,18 @@ local class = require '../libs/middleclass'
 Room = class('Room')
 
 function Room:initialize(name)
+	-- get the name of the room to load
 	self.name = name
+
+	-- try to open the sroom file
+	local room_path = "rooms/" .. self.name .. ".sroom"
+	if (love.filesystem.exists(room_path)) then
+		-- found the sroom file
+	else
+		print("ERROR: Failed to load room " .. name)
+		print("Check the name and extension of the room you're trying to load")
+		love.event.quit(1)
+	end
 end
 
 function Room:update(dt)
